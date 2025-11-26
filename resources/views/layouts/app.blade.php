220a0601f1656d5ed3264eaa35d5018a028cfa47
sortsom <62197355+sortsom@users.noreply.github.com>
2025-11-26 20:02:02 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,6 +42,7 @@
         <div class="page-wrapper">
             {{ $slot }}
         </div>
+        <x-footer />
     </div>
     <script src="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta20/dist/js/tabler.min.js"></script>
 </body>
